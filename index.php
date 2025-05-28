1ab793757ff218964b5d8ec42e2b8f67d49cc1bf
HARADA Kazuo <harakazu1968@gmail.com>
2025-05-28 13:57:08 +0900

    画像の追加

--- index.php
+++ index.php
@@ -30,7 +30,26 @@
     </div>
   </header>
 </body <?php body_class(); ?>>
-
+<section class="intro">
+  <div class="inner">
+    <div class="introContainer">
+      <h2 class="introLead">
+        ヒアリング重視の<br>目的に合った最適な提案
+      </h2>
+      <p class="introDisc">
+        WEB制作会社のSAMPLE site.です。<br> 企業サイトや採用サイトの制作、Wordpress構築と<br> WEBマーケティングを得意としています。
+      </p>
+    </div>
+  </div>
+</section>
+<section id="about" class="about">
+  <div class="inner">
+    <div class="sectionTitle">
+      <p>ABOUT</p>
+      <h2>私たちについて</h2>
+    </div>
+  </div>
+</section>
 </body>
 
 </html>
